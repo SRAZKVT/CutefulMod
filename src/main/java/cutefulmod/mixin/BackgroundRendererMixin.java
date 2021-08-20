497fd0b3c49c4e187791bf54597f530ffc8ff4a8
Maheesha <maheeshasithum@gmail.com>
2021-08-20 15:41:15 +0530

    fixing issues with fog

--- src/main/java/cutefulmod/mixin/BackgroundRendererMixin.java
+++ src/main/java/cutefulmod/mixin/BackgroundRendererMixin.java
@@ -1,5 +1,6 @@
 package cutefulmod.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import cutefulmod.config.Configs;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
@@ -13,6 +14,8 @@ public class BackgroundRendererMixin {
     @Inject(method = "applyFog", at = @At(value = "HEAD"), cancellable = true)
     private static void renderNoFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, CallbackInfo ci) {
         if (Configs.getInstance().renderNoFog.value) {
+            RenderSystem.setShaderFogStart(1028);
+            RenderSystem.setShaderFogEnd(2088);
             ci.cancel();
         }
     }
