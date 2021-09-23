2d48cb8736a7fbb86b97b9c96dea617667e95a56
Sarah-Mathey <sarah.mathey66666@gmail.com>
2021-09-23 19:19:36 +0200

    added /stone command

---
+++ src/main/java/cutefulmod/commands/StoneCommand.java
@@ -0,0 +1,19 @@
+package cutefulmod.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+
+
+public class StoneCommand {
+    public static void registerCommand (CommandDispatcher<ServerCommandSource> dispatcher) {
+    dispatcher.register(CommandManager.literal("stone").executes((commandContext) -> execute()));
+    }
+
+    private static int execute() {
+        assert MinecraftClient.getInstance().player != null;
+        MinecraftClient.getInstance().player.sendChatMessage("/setblock ~ ~-1 ~ stone");
+        return 1;
+    }
+}
---
+++ src/main/java/cutefulmod/mixin/CommandManagerMixin.java
@@ -0,0 +1,25 @@
+package cutefulmod.mixin;
+
+import com.mojang.brigadier.CommandDispatcher;
+import cutefulmod.commands.StoneCommand;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CommandManager.class)
+public class CommandManagerMixin {
+    @Shadow @Final private CommandDispatcher<ServerCommandSource> dispatcher;
+
+    @Inject(
+            method = "<init>",
+            at = @At(value = "TAIL")
+    )
+    private void injectCommands(boolean isDedicatedServer, CallbackInfo ci) {
+        StoneCommand.registerCommand(this.dispatcher);
+    }
+}
