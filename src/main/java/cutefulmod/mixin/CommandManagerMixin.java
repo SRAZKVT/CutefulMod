3659d77bc1b5c64ea91dd47ca4db392f6e78f156
Maheesha <maheeshasithum@gmail.com>
2021-09-28 16:28:20 +0530

    added the stone command to 1.17 branch

---
+++ src/main/java/cutefulmod/mixin/CommandManagerMixin.java
@@ -0,0 +1,27 @@
+package cutefulmod.mixin;
+
+import com.mojang.brigadier.CommandDispatcher;
+import cutefulmod.commands.StoneCommand;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CommandManager.class)
+public class CommandManagerMixin {
+    @Shadow
+    @Final
+    private CommandDispatcher<ServerCommandSource> dispatcher;
+
+    @Inject(
+            method = "<init>",
+            at = @At(value = "TAIL")
+    )
+    private void injectCommands(CommandManager.RegistrationEnvironment environment, CallbackInfo ci) {
+        StoneCommand.registerCommand(this.dispatcher);
+    }
+}
