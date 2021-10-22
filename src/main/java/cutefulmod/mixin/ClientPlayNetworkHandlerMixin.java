77f668485551b23550ea0a2c79b77945a45f22ec
Sarah-Mathey <sarah.mathey66666@gmail.com>
2021-10-22 18:50:20 +0200

    added error handler for commands syntax
    this way game prints error in case of syntax issue, instead of just crashing

--- src/main/java/cutefulmod/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/cutefulmod/mixin/ClientPlayNetworkHandlerMixin.java
@@ -6,11 +6,15 @@ import cutefulmod.commands.RayCountCommand;
 import cutefulmod.commands.StoneCommand;
 import cutefulmod.config.Configs;
 import cutefulmod.util.CutefulUtils;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.command.CommandException;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.ChatMessageC2SPacket;
 import net.minecraft.network.packet.s2c.play.CommandTreeS2CPacket;
 import net.minecraft.server.command.CommandSource;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -23,6 +27,8 @@ public class ClientPlayNetworkHandlerMixin {
     @Shadow
     private CommandDispatcher<CommandSource> commandDispatcher;
 
+    @Shadow private MinecraftClient client;
+
     // taken from CutelessMod https://github.com/Nessiesson/CutelessMod but added case for /execute
     @Inject(
             method = "sendPacket",
@@ -33,33 +39,46 @@ public class ClientPlayNetworkHandlerMixin {
         if (packet instanceof ChatMessageC2SPacket) {
             String message = ((ChatMessageC2SPacket) packet).getChatMessage();
             String[] words = message.split(" ");
-            switch (words[0]) {
-                case "/tp":
-                    Configs.updateLastPosDim();
-                    break;
-                case "/execute":
-                    for (int i = 1; i < words.length - 2; i++) {
-                        if (words[i].equals("run") && words[i + 1].equals("tp")) {
-                            Configs.updateLastPosDim();
-                            break;
+            try {
+                switch (words[0]) {
+                    case "/tp":
+                        Configs.updateLastPosDim();
+                        break;
+                    case "/execute":
+                        for (int i = 1; i < words.length - 2; i++) {
+                            if (words[i].equals("run") && words[i + 1].equals("tp")) {
+                                Configs.updateLastPosDim();
+                                break;
+                            }
                         }
-                    }
-                    break;
-                case "/raycount":
-                    BlockPos pos = CutefulUtils.getBlockPosFromStrings(words[1], words[2], words[3]);
-                    if (pos != null) {
-                        RayCountCommand.execute(pos);
-                    }
-                    ci.cancel();
-                    break;
-                case "/back":
-                    BackCommand.execute();
-                    ci.cancel();
-                    break;
-                case "/stone":
-                    StoneCommand.execute();
-                    ci.cancel();
-                    break;
+                        break;
+                    case "/raycount":
+                        if (words.length > 3) {
+                            BlockPos pos = CutefulUtils.getBlockPosFromStrings(words[1], words[2], words[3]);
+                            if (pos != null) {
+                                RayCountCommand.execute(pos);
+                            }else {
+                                throw new CommandException(new LiteralText("Please enter a valid position"));
+                            }
+                        } else {
+                            throw new CommandException(new LiteralText("Please enter a valid position"));
+                        }
+                        ci.cancel();
+                        break;
+                    case "/back":
+                        BackCommand.execute();
+                        ci.cancel();
+                        break;
+                    case "/stone":
+                        StoneCommand.execute();
+                        ci.cancel();
+                        break;
+                }
+            } catch (CommandException e) {
+                String error = e.getMessage();
+                assert this.client.player != null;
+                this.client.player.sendMessage((new LiteralText("")).append(error).formatted(Formatting.RED));
+                ci.cancel();
             }
         }
     }
